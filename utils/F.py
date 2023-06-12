1a62fa677ea81c4220f3eb456365f27599e1bbe0
tony <tony>
2023-06-12 17:39:37 +0800

    add 生成具有隨機訊息 的 QR Code

--- utils/F.py
+++ utils/F.py
@@ -2,6 +2,8 @@ import os
 import shutil
 from datetime import datetime
 import torch
+import random
+import qrcode
 
 
 def write_log(log_str, log_file):
@@ -36,3 +38,87 @@ def ensure_folder(folder_path, remake=False):
             os.makedirs(folder_path, 0o755)
     else:
         os.makedirs(folder_path, 0o755)
+        
+        
+def generate_random_message(qr_version):
+    """
+    取得符合對應 qrcode version 的 message 長度。
+    """
+    
+    # Set the maximum length limit for each QR code version
+    version_limits = {
+        1: 25,
+        2: 47,
+        3: 77,
+        4: 114,
+        5: 154,
+        6: 195,
+        7: 224,
+        8: 279,
+        9: 335,
+        10: 395,
+        11: 468,
+        12: 535,
+        13: 619,
+        14: 667,
+        15: 758,
+        16: 854,
+        17: 938,
+        18: 1046,
+        19: 1153,
+        20: 1249,
+        21: 1352,
+        22: 1460,
+        23: 1588,
+        24: 1704,
+        25: 1853,
+        26: 1990,
+        27: 2132,
+        28: 2223,
+        29: 2369,
+        30: 2520,
+        31: 2677,
+        32: 2840,
+        33: 3009,
+        34: 3183,
+        35: 3351,
+        36: 3537,
+        37: 3729,
+        38: 3927,
+        39: 4087,
+        40: 4296
+    }
+    # Check if the QR code version is valid
+    if qr_version < 1 or qr_version > 40:
+        raise ValueError("Invalid QR version. Valid versions are 1 to 40.")
+
+    # Get the maximum length limit for the specified version
+    max_length = version_limits[qr_version]
+    min_length = version_limits[qr_version-1] if qr_version!= 1 else version_limits[1]
+    # Generate a random message
+    message_length = random.randint(max_length // 2, max_length)  # Randomly choose the message length
+    random_message = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=message_length))
+
+    return random_message
+
+
+def generate_std_qr(FOLDER, NUMBER_OF_QR):
+    # FOLDER 存在哪邊?
+    # NUMBER_OF_QR 生幾張?
+    if not os.path.isdir(FOLDER):
+        os.mkdir(FOLDER)
+
+    # Adjusted sample size to fit the population
+    for data in range(NUMBER_OF_QR):
+        version =  random.choice(range(1, 14))
+        qr = qrcode.QRCode(
+            version=version,
+            error_correction=qrcode.constants.ERROR_CORRECT_M,
+            box_size=10,
+            border=1
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
+        image = qr.make_image(fill_color="black", back_color="white")
+        filename = f"{FOLDER}/QR_Version_{version}_Data_{data}.png"
+        image.save(filename)
